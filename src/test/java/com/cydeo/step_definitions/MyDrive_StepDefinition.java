8ef1244e881170675a4eb1c21988635a8b9558eb
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-29 14:46:00 +0300

    MyDrive_StepDefinition

--- src/test/java/com/cydeo/step_definitions/MyDrive_StepDefinition.java
+++ src/test/java/com/cydeo/step_definitions/MyDrive_StepDefinition.java
@@ -13,7 +13,7 @@ import org.junit.Assert;
 public class MyDrive_StepDefinition {
 
     AgileProCrmLoginPage agileProCrmLoginPage = new AgileProCrmLoginPage();
-    DrivePage mainPage= new DrivePage();
+    DrivePage drivePage = new DrivePage();
     @Given("User is already with {string} and {string} logged in and on Activity Stream page")
     public void user_is_already_with_and_logged_in_and_on_activity_stream_page(String username, String password) {
         Driver.getDriver().get(ConfigurationReader.getProperty("agileprocrm.url"));
@@ -21,15 +21,33 @@ public class MyDrive_StepDefinition {
     }
     @When("User clicks Drive menu item")
     public void user_clicks_drive_menu_item() {
-        mainPage.driveMenuitem.click();
+        drivePage.driveMenuitem.click();
     }
     @Then("User should be able to see title is My Drive")
     public void user_should_be_able_to_see_title_is_my_drive() {
         String expectedText= "My Drive";
         BrowserUtils.sleep(1);
-        String actualText= mainPage.myDriveText.getText();
+        String actualText= drivePage.myDriveText.getText();
         Assert.assertEquals("My Drive is not display",expectedText,actualText);
 
     }
+
+
+    @When("User clicks Company Drive tab")
+    public void user_clicks_company_drive_tab() {
+
+        drivePage.companyDriveField.click();
+
+
+    }
+    @Then("User should be able to display Company drive window")
+    public void user_should_be_able_to_display_company_drive_window() {
+
+        String expectedText= "Company drive";
+        BrowserUtils.sleep(1);
+        String actualText= drivePage.companyDriveText.getText();
+        Assert.assertEquals("Company drive is not display",expectedText,actualText);
+    }
+
 
 }
