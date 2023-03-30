d45df986a6d113e3884e035924aa221ab5fc648a
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-30 23:32:37 +0300

    final

--- src/test/java/com/agileProCrm/step_definitions/Hooks.java
+++ src/test/java/com/agileProCrm/step_definitions/Hooks.java
@@ -1,11 +1,11 @@
-package com.cydeo.step_definitions;
+package com.agileProCrm.step_definitions;
 
 /*
 In the class we will be able to pass pre- & post- conditions to each
 scenario and each step
  */
 
-import com.cydeo.utilities.Driver;
+import com.agileProCrm.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
